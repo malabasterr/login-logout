4cf6bd1cbbb725b215dabea60b3c03b33c3a3732
malabasterr <113557081+malabasterr@users.noreply.github.com>
2023-08-17 10:35:12 +0100

    first attempt, w/out dictionary

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleLogin = () => {
+
+    if (username === 'user' && password === 'password') {
+      setIsLoggedIn(true);
+      setErrorMessage('');
+    } else {
+      setErrorMessage('Invalid username or password');
+    }
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setUsername('');
+    setPassword('');
+  };
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value); // accessing current value entered by the user
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value); // accessing current value entered by the user
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reloadddd.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isLoggedIn ? (
+        <div>
+          <p>Welcome, {username}!</p>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <button onClick={handleLogin}>Login</button>
+          <p>{errorMessage}</p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
